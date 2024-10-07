67c43be54e427c97eb62f614bf54975eab0fb873
Bijay Bartaula <vijaybartaula1@gmail.com>
2024-10-07 15:16:11 +0545

    first commit

---
+++ product detail page/script.js
@@ -0,0 +1,4 @@
+document.getElementById('add-to-cart').addEventListener('click', function() {
+    const quantity = document.getElementById('quantity').value;
+    alert(`Added ${quantity} item(s) to your cart!`);
+});
